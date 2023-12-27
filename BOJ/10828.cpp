a4b6e4af1e6a9a3ef5576fdd54679aefddd7c632
causyj <jainayj804998@gmail.com>
2023-12-27 23:01:40 +0900

    Solved 10828

---
+++ BOJ/10828.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    stack<int> s;
+    for(int i=0;i<N; i++){
+        string c;
+        cin >> c;
+        if(c == "push"){
+            int p;
+            cin >> p;
+            s.push(p);
+        }
+        else if(c=="pop"){
+            if(s.empty()){
+                cout << -1 << '\n';
+            }
+            else{
+                cout << s.top() << '\n';
+                s.pop();
+
+            }
+        }
+        else if(c=="empty"){
+            cout << s.empty() << '\n';
+        }
+        else if(c=="size"){
+            cout << s.size() << '\n';
+        }
+        else if(c=="top"){
+            cout << s.top() << '\n';
+        }
+    }
+    return 0;
+}
